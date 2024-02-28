3fae59f83ce7de6757a7d44553c8a841edc9da75
Max <maksim.bessonov2@gmail.com>
2024-02-28 12:24:59 +0200

    start

--- main.py
+++ main.py
@@ -14,23 +14,29 @@ acl_new="acl_new.txt"
 f_acl_new = open(acl_new, 'r')
 f_acl_current = open(acl_current, 'r')
 
-d_acl_new = {}
-d_acl_current = {}
-acl_number = 0
-acl_list = []
-for line in f_acl_new:
-    if "no access-list" in line:
-        d_acl_new[acl_number]= acl_list
-        acl_number = line.split()[2]
-        #print(acl_list)
-        acl_list = []
-    else:
-        acl_list.append(line)
-        #print(acl_list)
-d_acl_new[acl_number]= acl_list
-#print(d_acl_new)
+def acl_file_to_dict(file):
+    acl_number = 0
+    acl_list = []
+    acl_dict = {}
+    f_acl = open(file, 'r')
+    for line in f_acl:
+        if "no access-list" in line:
+            acl_dict[acl_number]= acl_list
+            acl_number = line.split()[2]
+            #print(acl_list)
+            acl_list = []
+        else:
+            acl_list.append(line)
+            #print(acl_list)
+    acl_dict[acl_number]= acl_list
+    f_acl.close()
+    return(acl_dict)
 
-for key in d_acl_new:
-    print(str(d_acl_new[key]))
+if __name__ == '__main__':
+    acl_dict_new = acl_file_to_dict(acl_new)
+    acl_dict_current = acl_file_to_dict(acl_current)
+
+for key in acl_dict_new:
+    print(str(acl_dict_new[key]))
     print()
 
